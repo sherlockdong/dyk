2da84597f8647d2187db7fe2721d244c609a8817
sherlockdong <146029448+sherlockdong@users.noreply.github.com>
2024-06-15 00:39:41 +0800

    blog

--- src/app/project/blog.js
+++ src/app/project/blog.js
@@ -2,7 +2,7 @@ export const sculptureList = [{
     title: 'My First ever blog',
     content:'This serves as an experiment. It is meant to test if the things are working fine.'
   }, {
-    title: 'fdsafdsa',
+    title: 'My second ever blog',
     content:'angeldfas di maria'
   }, {
     title: 'fdsafdsa',
