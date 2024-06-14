57b1b5566d9ad4096578db141d3ec38324798046
sherlockdong <146029448+sherlockdong@users.noreply.github.com>
2024-06-15 00:56:31 +0800

    header

--- src/comps/header.js
+++ src/comps/header.js
@@ -4,9 +4,9 @@ const Header =()=>{
        <div id = "header">
         <Link href="/">Sherlock Dong</Link>
             <nav id="right">
-                <Link href="/project" > <h2>Project <span>-&gt;</span></h2></Link>    
+                <Link href="/projects" > <h2>Projects <span>-&gt;</span></h2></Link>    
                 <h2>Images <span>-&gt;</span></h2>
-                <h2>Stories <span>-&gt;</span></h2>
+                <Link href="/stories" > <h2>Stories <span>-&gt;</span></h2></Link> 
                 <h2>Books I read <span>-&gt;</span></h2>
             </nav>
        </div>
