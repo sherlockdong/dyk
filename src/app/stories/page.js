48095f4babd84b50aa4e11e1acfb7b7290a6f356
sherlockdong <146029448+sherlockdong@users.noreply.github.com>
2024-06-19 21:07:30 +0800

    back

--- src/app/stories/page.js
+++ src/app/stories/page.js
@@ -6,12 +6,19 @@ export default function Gallery() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
   const hasNext = index < sculptureList.length - 1;
-
+  const hasBack = index >= 1;
   function handleNextClick() {
     if (hasNext) {
       setIndex(index + 1);
     } else {
       setIndex(0);
+    }
+  }
+  function handleBackClick() {
+    if (hasBack) {
+      setIndex(index - 1);
+    } else {
+      setIndex(sculptureList.length-1);
     }
   }
 
@@ -23,11 +30,13 @@ export default function Gallery() {
   return (
     <><div id='st'>
       <h2> {sculpture.title}</h2>
-      <h3 id='lis'>
+      <h3 id = 'leng'>
         ({index + 1} of {sculptureList.length})
       </h3>
       <p id="stories" > {sculpture.content}</p>
       <p id='ftn'> {sculpture.footn}</p>
+      <button id='sculeft' onClick={handleBackClick}><span>
+      Back</span></button>
       <button id='scul' onClick={handleNextClick}><span>
         Next</span>
       </button>
