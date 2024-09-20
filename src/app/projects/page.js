9d9dd6e8cfd1f7b3d9c1ac7c79aa324ef5660c61
sherlockdong <sherlockdong2007@gmail.com>
2024-09-19 21:32:10 -0400

    proj

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,11 +2,8 @@
 import Link from 'next/link'
 export default function proj() {
     return(
-      <>
-      <Link  href="https://pho-guide.com"
-      target="_blank"
-      rel="noopener noreferrer"><h2>PHO - Guide  <span>-&gt;</span></h2> </Link>
-      <p>Proudly present my project, Physics Olympiad Guide, to dedicated physics students.</p>
+      <>Most of my projects are still underp-development, many of which are really exciting. Please look forward to it!
+      
 
       
 </>
