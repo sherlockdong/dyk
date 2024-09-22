444f782332b5ad322e5ccb463e69ce6b418bf046
sherlockdong <sherlockdong2007@gmail.com>
2024-09-22 19:53:50 -0400

    nf

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 const  NotFound = ()=>{
-    return (<p id="NF"> Page not Found.
+    return (<p id="NF"> Page not Found. Most likely, this page is under development.
 
     <br />Back to main Page <Link href="/">Home</Link>
     </p>)
--- src/comps/header.js
+++ src/comps/header.js
@@ -5,9 +5,9 @@ const Header =()=>{
         <Link href="/">Sherlock Dong</Link>
             <nav id="right">
                 <Link href="/projects" > <h2>Projects <span>-&gt;</span></h2></Link>    
-                <h2>Images <span>-&gt;</span></h2>
+                <Link href="/images" > <h2>Images <span>-&gt;</span></h2></Link>    
                 <Link href="/stories" > <h2>Stories <span>-&gt;</span></h2></Link> 
-                <Link href="/books" > <h2>Stories <span>-&gt;</span></h2></Link> 
+                <Link href="/books" > <h2>Books <span>-&gt;</span></h2></Link> 
             </nav>
        </div>
     )
