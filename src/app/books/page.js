9a7a6a1d28d0d9c65cf01ac7e1bfe7b7806f2970
sherlockdong <sherlockdong2007@gmail.com>
2024-09-25 20:43:18 -0400

    books

--- src/app/books/page.js
+++ src/app/books/page.js
@@ -1,7 +1,19 @@
 'use client';
 const Book = () => {
     return (
-        <h2>Grit</h2>
+        <div id='books'>{
+           <><h2>Grit</h2><p>This is a book I read over the summer of 2024. Personally, I would not recommend the book to teenagers,
+            because being able to understand what the author is talking about requires a lot of experiences in life, 
+            so for me, I only rememberes some points that she was trying to make in the book. However, I would highly recommend
+            it to the parents, educators and most of the people who are working. This book really makes me think about how I would
+            educate my kids as a father, and how I would teach my students as a professor, to make them be gritty on the things they 
+            are trying to do. 
+                
+                
+                
+         </p></>
+        }
+        </div>
     )
 }
 export default Book;
