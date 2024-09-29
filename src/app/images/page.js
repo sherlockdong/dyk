a7191dd899fe878486582644ac817ba6d8d3a502
sherlockdong <sherlockdong2007@gmail.com>
2024-09-29 13:54:47 -0400

    homepageandimage

--- src/app/images/page.js
+++ src/app/images/page.js
@@ -1,9 +1,15 @@
 'use client'
 
+
 const imag= () => {
-    return (
+    return (<>
+    <img src="./IMG_5915.jpg" alt="Description of the image" width="500" height="auto" />
+
         <div id='tet'><p>This page is quite similar to an instagram page. The only difference is that, here I post more pictures than I do on my instagram
-            account.</p></div>
+            account.</p></div><div id="beijingTime"></div>
+
+            
+            </> 
     )
 }
 export default imag;
--- src/app/page.js
+++ src/app/page.js
@@ -35,6 +35,40 @@ export default function Home() {
           </h2>
           <p> I enjoy reading books, and I write reviews for some of my favorite books.</p></Link>
       </div>
+      <div className={styles.lag}>
+      <h2>Language Spoken</h2>
+      <p>below are the archives of the artiles I wrote in the following languages.</p></div>
+      <div className={styles.sec1}>
+<div className={styles.frn}>
+<Link href='/fra'>
+  <h3>French</h3>
+  <p>I started learning French at the age of 15.</p>
+</Link></div>
+
+<Link href='/chn' className={styles.mad}>
+  <h3>Chinese</h3>
+  <p>This is my first language, and I continue to use this language as well.</p>
+</Link>
+
+<Link href='/eng' className={styles.eng}>
+  <h3>English</h3>
+  <p>I started to speak English at 4, and now it is the language I use the most.</p>
+</Link>
+      </div>
+  
+      <div className={styles.lag}>
+      <h2>Education</h2>
+      <h3>Elementary School</h3>
+      <h4><Link href='http://www.jzhx.net/xxb/' target='_blank'>2014-2019: Nanjing Jinlin Highschool Hexi Campus '2020</Link></h4>
+      <h4><Link href='http://www.nflsxl.com/7/list.htm' target='_blank'>2019-2020: Nanjing Foreign Language School Xianlin Campus '2020</Link></h4>
+      <h3>Middle School</h3>
+      <h4><Link href='http://www.nfls.com.cn/' target='_blank'>2020-2023: Nanjing Foreign Language School '2023</Link></h4>
+      <h3>High School</h3>
+      <h4><Link href='https://www.berkshireschool.org/' target='_blank'>2023-2027: Berkshire School '2027</Link></h4>
+      </div>
+
+
     </main>
   );
 }
+
