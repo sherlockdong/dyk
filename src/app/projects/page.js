3c8ffa93b5c0ecd54c9dc13675faaf6d2c06421d
sherlockdong <sherlockdong2007@gmail.com>
2025-02-26 15:47:14 -0500

    p5

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,7 +2,6 @@
 import Link from 'next/link'
 import ChatComponent from "../../components/chat";
 
-import PhysicsSimulator from "../physt/PhysicsSimulator";
 export default function proj() {
     return(
       <><div id='tet'><p>Most of my projects are still underp-development, many of which are really exciting. Please look forward to it!</p></div>
